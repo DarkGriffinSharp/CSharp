74942a720c0c731882a2d20528aaf72c05967c14
DarkGriffinSharp <36265627+DarkGriffinSharp@users.noreply.github.com>
2018-02-09 00:14:28 +0300

    Simple Processor
    Basic DarkGriffin Process info etc

---
+++ SimpleProcessor/SimpleProcessor/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace SimpleProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.findButton = new System.Windows.Forms.Button();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.starterButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(208, 304);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.AutoEllipsis = true;
+            this.refreshButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.refreshButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.refreshButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.refreshButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.refreshButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.refreshButton.Location = new System.Drawing.Point(208, 0);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(403, 43);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(208, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_Click);
+            // 
+            // findButton
+            // 
+            this.findButton.AutoEllipsis = true;
+            this.findButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.findButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.findButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.findButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.findButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.findButton.Location = new System.Drawing.Point(208, 66);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(403, 43);
+            this.findButton.TabIndex = 4;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.processFind_Click);
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // starterButton
+            // 
+            this.starterButton.AutoEllipsis = true;
+            this.starterButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.starterButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.starterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.starterButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.starterButton.Location = new System.Drawing.Point(208, 109);
+            this.starterButton.Name = "starterButton";
+            this.starterButton.Size = new System.Drawing.Size(403, 43);
+            this.starterButton.TabIndex = 5;
+            this.starterButton.Text = "Starter";
+            this.starterButton.UseVisualStyleBackColor = true;
+            this.starterButton.Click += new System.EventHandler(this.starterButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(208, 152);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(403, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(208, 180);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(611, 304);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.starterButton);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DarkGriffin - Processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Timer refreshTimer;
+        private System.Windows.Forms.Button starterButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SimpleProcessor/SimpleProcessor/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace SimpleProcessor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshTimer.Start();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void processFind_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox1.Text != " ")
+            {
+                int index = listBox1.FindString(textBox1.Text);
+                listBox1.SelectedIndex = index;
+            }
+            else
+            {
+                textBox1.Text = "It cannot be left blank.";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.SelectedItem.ToString();
+            comboBox1.Items.Clear();
+            int index = listBox1.SelectedIndex, count = 0;
+            Process[] processList = Process.GetProcesses();
+            foreach (Process pro in processList)
+            {
+                if (index == count)
+                {
+                    if (pro.Id != 0)
+                    {
+                        comboBox1.Text = pro.ProcessName;
+                        comboBox1.Items.Add("Physical Memory= " + pro.WorkingSet64);
+                        comboBox1.Items.Add("base priority= " + pro.BasePriority);
+                        try
+                        {
+                            comboBox1.Items.Add("priority class= " + pro.PriorityClass);
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                comboBox1.Items.Add("user processor time= " + pro.UserProcessorTime);
+                            }
+                            catch (Exception)
+                            {
+                                comboBox1.Items.Add("privileged processor time= " + pro.PrivilegedProcessorTime);
+                                comboBox1.Items.Add("total processor time= " + pro.TotalProcessorTime);
+                                comboBox1.Items.Add("PagedSystemMemorySize64= " + pro.PagedSystemMemorySize64);
+                                comboBox1.Items.Add("PagedMemorySize64= " + pro.PagedMemorySize64);
+                            }
+                            comboBox1.Items.Add("privileged processor time= " + pro.PrivilegedProcessorTime);
+                            comboBox1.Items.Add("total processor time= " + pro.TotalProcessorTime);
+                            comboBox1.Items.Add("PagedSystemMemorySize64= " + pro.PagedSystemMemorySize64);
+                            comboBox1.Items.Add("PagedMemorySize64= " + pro.PagedMemorySize64);
+                            return;
+                        }
+                        comboBox1.Items.Add("user processor time= " + pro.UserProcessorTime);
+                        comboBox1.Items.Add("privileged processor time= " + pro.PrivilegedProcessorTime);
+                        comboBox1.Items.Add("total processor time= " + pro.TotalProcessorTime);
+                        comboBox1.Items.Add("PagedSystemMemorySize64= " + pro.PagedSystemMemorySize64);
+                        comboBox1.Items.Add("PagedMemorySize64= " + pro.PagedMemorySize64);
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                count++;
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            int count = 0;
+            Process[] processList = Process.GetProcesses();
+            foreach (Process proc in processList)
+            {
+                if (index == count)
+                {
+                    proc.Kill();
+                    break;
+                }
+                else
+                {
+                    count++;
+                    continue;
+                }
+            }
+            refreshTimer.Start();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            Process[] processList = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach (Process processes in processList)
+            {
+                if (processes.Id == 0)
+                {
+                    listBox1.Items.Add("System Idle" + " - 0");
+                }
+                else
+                {
+                    listBox1.Items.Add(processes.ProcessName + " - " + processes.Id);
+                }
+            }
+            refreshTimer.Stop();
+        }
+
+        private void refreshButton_Click_1(object sender, EventArgs e)
+        {
+            refreshTimer.Start();
+        }
+
+        private void starterButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ShowDialog(); // dosyayı seçme diyaloğunu açıyor
+            string name = fileDialog.FileName; // name değişkenine seçilen dosyanın url atıyor
+            try
+            {
+                Process.Start(name); // belirtilen url'deki dosyayı çalıştırıyor
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
