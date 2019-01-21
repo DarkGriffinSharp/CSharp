3af2860de03bd6d7c99d50e45bc0ac8ef544ccc5
DarkGriffinSharp <36265627+DarkGriffinSharp@users.noreply.github.com>
2019-01-21 12:49:19 +0300

    SqlBaglantisi

---
+++ SQLConnections/SQLConnections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SQLConnections
+{
+    class Program
+    {
+        // Bağlantı konumunu almak için Server Explorer'dan bağlantı oluşturma gerekli.
+        static string path = @"Data Source=302--12;Initial Catalog=demoDB;Persist Security Info=True;User ID=sa;Password=1234";
+
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(path);
+            try
+            {
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Bağlantı başarılı");
+                }
+                else
+                {
+                    Console.WriteLine("Bağlantı başarısız.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
